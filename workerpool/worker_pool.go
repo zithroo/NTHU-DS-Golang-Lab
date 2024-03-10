1b8256dc4f1536c819813d0c02c1e32ff60cc566
zithroo <zithroo@gmail.com>
2024-03-10 07:27:13 +0000

    feat: complete start & run

---
+++ workerpool/worker_pool.go
@@ -0,0 +1,82 @@
+package workerpool
+
+import (
+	"context"
+	"sync"
+)
+
+type Task struct {
+	Func func(args ...interface{}) *Result
+	Args []interface{}
+}
+
+type Result struct {
+	Value interface{}
+	Err   error
+}
+
+type WorkerPool interface {
+	Start(ctx context.Context)
+	Tasks() chan *Task
+	Results() chan *Result
+}
+
+type workerPool struct {
+	numWorkers int
+	tasks      chan *Task
+	results    chan *Result
+	wg         *sync.WaitGroup
+}
+
+var _ WorkerPool = (*workerPool)(nil)
+
+func NewWorkerPool(numWorkers int, bufferSize int) *workerPool {
+	return &workerPool{
+		numWorkers: numWorkers,
+		tasks:      make(chan *Task, bufferSize),
+		results:    make(chan *Result, bufferSize),
+		wg:         &sync.WaitGroup{},
+	}
+}
+
+func (wp *workerPool) Start(ctx context.Context) {
+	// TODO: implementation
+	//
+	// Starts numWorkers of goroutines, wait until all jobs are done.
+	// Remember to closed the result channel before exit.
+	for i:=0; i<wp.numWorkers; i++ {
+    	wp.wg.Add(1)
+        go wp.run(ctx)
+    }
+
+    wp.wg.Wait()
+    close(wp.Results())
+}
+
+func (wp *workerPool) Tasks() chan *Task {
+	return wp.tasks
+}
+
+func (wp *workerPool) Results() chan *Result {
+	return wp.results
+}
+
+func (wp *workerPool) run(ctx context.Context) {
+	// TODO: implementation
+	//
+	// Keeps fetching task from the task channel, do the task,
+	// then makes sure to exit if context is done.
+	defer wp.wg.Done()
+    for {
+        select {
+        case task, ok := <- wp.Tasks():
+           	if !ok {
+                return
+            }
+			result := task.Func(task.Args...)
+            wp.Results() <- result
+        case <- ctx.Done():
+            return
+        }
+    }
+}
